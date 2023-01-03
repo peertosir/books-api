e921b0064f250cf74d2a5cfec35ee8284732a1f3
peertosir <peertosir@gmail.com>
2023-01-04 01:59:26 +0300

    Closes #1. add db support + initial migrations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +18,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -26,9 +36,25 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (dev|stage|prod)")
+
+	// DB
+	flag.StringVar(&cfg.db.dsn, "dsn", os.Getenv("PGSQL_DSN"), "PGSQL connection string")
+	flag.IntVar(&cfg.db.maxOpenConns, "max-open-conns", 25, "PGSQL max open conns")
+	flag.IntVar(&cfg.db.maxIdleConns, "max-idle-conns", 25, "PGSQL max idle conns")
+	flag.StringVar(&cfg.db.maxIdleTime, "max-idle-time", "15m", "PGSQL max idle time")
+
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	defer db.Close()
+
+	logger.Printf("database connection pool esteblished")
 
 	app := &application{
 		config: cfg,
@@ -44,6 +70,31 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %d", cfg.env, cfg.port)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
