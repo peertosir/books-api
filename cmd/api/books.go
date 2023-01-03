b02e3bd7ccb68774f5aed8bb556ac25d763037c2
peertosir <peertosir@gmail.com>
2023-01-03 23:33:13 +0300

    Add some validation for input JSON

--- cmd/api/books.go
+++ cmd/api/books.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/peertosir/books-api/internal/data"
+	"github.com/peertosir/books-api/internal/validator"
 )
 
 func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +20,27 @@ func (app *application) createBookHandler(w http.ResponseWriter, r *http.Request
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	v.Check(input.Title != "", "title", "must be provided")
+	v.Check(len(input.Title) <= 500, "title", "must not be longer than 500 bytes")
+
+	v.Check(input.Year != 0, "year", "must be provided")
+	v.Check(input.Year <= int32(time.Now().Year()), "year", "must not be in future")
+
+	v.Check(input.Pages != 0, "pages", "must be provided")
+	v.Check(input.Pages > 0, "pages", "must be > 0")
+
+	v.Check(input.Genres != nil, "genres", "must be provided")
+	v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 entry")
+	v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 entries")
+	v.Check(v.Unique(input.Genres), "genres", "must contain unique entries")
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
